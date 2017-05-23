dd66875987a3848686f0e1b513ba075f86b9bd48
TimothyJohnK <Tim@VectorLab.com>
2017-05-23 14:09:27 -0500

    SPA not working yet; first commit to branch

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 window.addEventListener('load', function () {
+  var songList = document.getElementsByClassName('navBar-songlist')[0].style.display,
+    email = document.getElementsByClassName('email')[0];
+    songSection = document.getElementsByClassName('song-section')[0].style.display;
 
+  
+  songList.addEventListener('load', function () {
+    document.getElementsByClassName('song-section')[0].style.display = 'none';
+  });
+  // songList.addEventListener('click', function () {
+  //   document.getElementsByClassName('song-section')[0].style.display = 'flex';
+  });
 };
